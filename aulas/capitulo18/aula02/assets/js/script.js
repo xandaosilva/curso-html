1f762e30e1d824b5fb978dbe678d822fd11a72a7
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-15 17:21:35 -0300

    Corrigindo exercício lógica de programação - aula 183

--- aulas/capitulo18/aula02/assets/js/script.js
+++ aulas/capitulo18/aula02/assets/js/script.js
@@ -13,5 +13,5 @@ if(x === y) alert("Empate");
 else{
     var aleatorio = parseInt(Math.random() * 2);
     alert("Número aleatório gerado: " + aleatorio);
-    (aleatorio === 0 && x < y || aleatorio === 0 && x > y) ? alert("Jogador 1 venceu") : alert("Jogador 2 venceu");
+    (aleatorio === 0 && x < y || aleatorio === 1 && x > y) ? alert("Jogador 1 venceu") : alert("Jogador 2 venceu");
 }
