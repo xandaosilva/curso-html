8b0000210e5d6f776395dfd2556dfc2f1e78367e
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-14 21:15:48 -0300

    Adicionando operações matemáticas no exercício inicial sobre javascript

--- aulas/capitulo18/aula01/assets/js/script.js
+++ aulas/capitulo18/aula01/assets/js/script.js
@@ -11,14 +11,18 @@ while(y === "")
 soma = x + y;
 subtracao = x - y;
 multiplicacao = x * y;
+potencia = Math.pow(x, y);
 
 alert("A soma é " + soma);
 alert("A subtração é " + subtracao);
 alert("A multiplicação é " + multiplicacao);
+alert("O número " + x + " elevado a " + y + " é: " + potencia);
 
 if(y === 0)
     alert("Impossível divisão por 0");
 else{
     divisao = x / y;
+    resto = x % y;
     alert("A divisão é " + divisao);
+    alert("O resto da divisão de " + x + " por " + y + " é: " + resto)
 }
