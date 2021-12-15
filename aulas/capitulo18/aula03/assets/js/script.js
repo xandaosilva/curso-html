1f1d15b65f73dce5cbf5fb0185c7f308c290ae3a
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-14 22:40:03 -0300

    Exercício switch case

---
+++ aulas/capitulo18/aula03/assets/js/script.js
@@ -0,0 +1,28 @@
+var dia = parseInt(prompt("Digite um número: "));
+
+switch(dia){
+    case 1:
+        alert("Domingo");
+        break;
+    case 2:
+        alert("Segunda-feira");
+        break;
+    case 3:
+        alert("Terça-feira");
+        break;
+    case 4:
+        alert("Quarta-feira");
+        break;
+    case 5:
+        alert("Quinta-feira");
+        break;
+    case 6:
+        alert("Sexta-feira");
+        break;
+    case 7:
+        alert("Sábado");
+        break;
+    default:
+        alert("Opção inválida");
+        break;
+}
