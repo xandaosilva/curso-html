a118999e74957ff42ea0a5554ea039b0c38400bc
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-15 20:00:35 -0300

    Exercício lógica de programação - Calculadora com funções

---
+++ aulas/capitulo20/aula01/assets/js/script.js
@@ -0,0 +1,29 @@
+var numero = parseFloat(prompt("Digite um número"));
+
+function mostrarInfo(parametro){
+    var resposta = document.querySelector("#container-resposta");
+    resposta.innerHTML = parametro;
+}
+
+function iniciarCalculo(operador){
+    var primeiroNumero = document.querySelector("#container-resposta").textContent;
+    var segundoNumero = parseFloat(prompt("Digite um número"));
+    var resposta = calcular(primeiroNumero, segundoNumero, operador);
+    mostrarInfo(resposta);
+}
+
+function calcular(primeiroNumero, segundoNumero, operador) {
+    var resposta = null;
+
+    if(operador === "+")
+        resposta = primeiroNumero + segundoNumero;
+    else if(operador === "-")
+        resposta = primeiroNumero + segundoNumero;
+    else if(operador === "*")
+        resposta = primeiroNumero + segundoNumero;
+    else
+        resposta = primeiroNumero + segundoNumero;
+    return resposta;
+}
+
+mostrarInfo(numero);
