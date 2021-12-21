3d02ec57fb14f32d3e4a1f6dc6073bdbf79c14ef
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-20 21:27:48 -0300

    Mostrar Link ativo em uma single page - final

--- aulas/capitulo36/aula01/assets/js/script.js
+++ aulas/capitulo36/aula01/assets/js/script.js
@@ -1,14 +1,41 @@
-var $teste = document.querySelector('.teste');
-console.log('bg: ', $teste.style.background);
-
-console.log('computado: ', getComputedStyle($teste).background)
-
-var $p = document.querySelector('p');
-console.log($p.offsetHeight, $p.offsetWidth);
-console.log($p.offsetLeft, $p.offsetTop);
-
-var $teste2 = document.querySelector('.teste2');
-
-console.log($teste2.offsetLeft, $teste2.offsetTop);
-
-console.log($teste.getBoundingClientRect())
+(function(){
+                
+    window.addEventListener('scroll', destacaMenu);
+    var $menu  = document.querySelector('.nav');
+    var $links = $menu.querySelectorAll('li a'); 
+    
+    function destacaMenu(){
+        var positions = [];
+        for(var i = 0; i < $links.length; i++){
+            positions[i] = pegaYPosicaoScroll($links[i])
+        }
+        console.log(positions);
+        
+        var linkAtual = pegaUltimoLinkVisivel(positions);
+        
+        var linkActived = $menu.querySelector('.actived');
+        if(linkActived) linkActived.classList.remove('actived');
+        
+        linkAtual.classList.add('actived');
+    }
+    
+    function pegaYPosicaoScroll(link){
+        var alvo = document.querySelector(link.getAttribute('href'));
+        var positionYViewport = alvo.getBoundingClientRect().top;
+        return positionYViewport;
+    }
+    
+    function pegaUltimoLinkVisivel(positions){
+        var qtd = positions.length;
+        while(qtd){
+            qtd--;
+            if(positions[qtd] < innerHeight / 2){                            
+                return $links[qtd];
+            }
+        }
+        return $links[0];
+        
+    }
+    
+    destacaMenu();
+})()
