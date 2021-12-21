1f665b3a9b53cfa1f69d9b4351e8cf16efe273cc
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-12-21 18:22:07 -0300

    JavaScript projeto Infusion - Menu - parte 2

--- aulas/capitulo37/aula01/assets/js/script.js
+++ aulas/capitulo37/aula01/assets/js/script.js
@@ -2,4 +2,7 @@
     var body = document.querySelector("body");
     body.classList.remove("no-js");
     body.classList.add("js");
+
+    var btn = document.querySelector(".header__btnMenu");
+    btn.removeAttribute("style");
 })();
